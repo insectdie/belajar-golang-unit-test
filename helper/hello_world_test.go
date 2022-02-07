2c77232684fd5cff122adc9e99d6d6a3a91e56f7
andryompusunggu <anho@ubslinux.com>
2022-02-07 21:46:23 +0700

    assert vs requiere. library : testify

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,7 +3,20 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Andry")
+	assert.Equal(t, "Hello Andryx", result, "Result must be 'Hello Andry'")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Andry")
+	assert.Equal(t, "Hello Andryx", result, "Result must be 'Hello Andry'")
+	fmt.Println("TestHelloWorldAndry Done")
+}
 
 func TestHelloWorldAndry(t *testing.T) {
 	result := HelloWorld("oke")
