136a924b567eabfc2bbe782b023b58d6ffa650da
insectdie <ompusungguandry@gmail.com>
2022-02-21 12:35:29 +0700

    Benchmark Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkHelloWorld(b *testing.B) {
+	for i:=0; i < b.N; i++ {
+		HelloWorld("Andry")
+	}
+}
+
 func TestTableHelloWorld(t *testing.T) {
 	test := []struct {
 		name     string
